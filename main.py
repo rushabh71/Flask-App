c828b49238c1539f2757dce1d05045d4959a2406
Rushabh Patel <rushabhp99@gmail.com>
2020-09-23 16:09:08 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from flask import Flask, render_template
-import pandas as pd
-import numpy as np
 import json
-import urllib.request
-from bs4 import BeautifulSoup
-import requests
-import re
-from datetime import datetime, date, timedelta
-import plotly.express as px
-import plotly.graph_objects as go
-import dash_table
 import os.path
+import re
+import urllib.request
+from datetime import date, datetime, timedelta
+
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
+import dash_table
+import numpy as np
+import pandas as pd
 import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template
 
 world_df = None;
 app = Flask(__name__)
@@ -40,7 +41,7 @@ def world_heatmap():
 
 	layout = dict(title='World Corona Cases')
 
-	graphJSON = json.dumps([data,layout], cls=plotly.utils.PlotlyJSONEncoder)
+	graphJSON = json.dumps([world_df['Country_Region'],'country names',world_df['Confirmed'],world_df['Country_Region'],['#55f262', '#f7cb63', '#f25c49']], cls=plotly.utils.PlotlyJSONEncoder)
 	return graphJSON
 
 
